ae90286997966707e88ebec472e97472189ccc4f
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-07-27 22:49:29 +0900

    コーディングミス修正
    * コーディングミス修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
     <head>
         <title>Book List</title>
     </head>
+    <script src="{{asset('/assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('/assets/js/bootstrap.min.js')}}"></script>
+    <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
     <body>
         <div class="container">
             <nav class="navbar navbar-default">
